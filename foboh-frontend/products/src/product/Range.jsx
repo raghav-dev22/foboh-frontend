8e9b5eabc60a62f3c00896630c66c0285f23e26a
sakshi jain <sjain302086@gmail.com>
2023-08-14 15:18:14 +0530

    product table  design reset

--- foboh-frontend/products/src/product/Range.jsx
+++ foboh-frontend/products/src/product/Range.jsx
@@ -20,7 +20,14 @@ import {
   Input,
 } from "@material-tailwind/react";
 import createArrayWithNumber from "../helpers/createArrayWithNumbers";
-const TABLE_HEAD = ["Title", "Code", "Configuration", "Price", "Stock level", " Status"];
+const TABLE_HEAD = [
+  "Title",
+  "Code",
+  "Configuration",
+  "Price",
+  "Stock level",
+  " Status",
+];
 function Range() {
   const [isBulkEdit, setIsBulkEdit] = useState(false);
   const [isDivVisible, setIsDivVisible] = useState(false);
@@ -28,6 +35,7 @@ function Range() {
   const [pages, setPages] = useState([]);
   const [page, setPage] = useState(1);
   const [selectedPage, setSelectedPage] = useState(0);
+  const [selectedProducts, setSelectedProducts] = useState([]);
   const navigate = useNavigate();
 
   const TABLE_ROWS = [
@@ -84,23 +92,26 @@ function Range() {
   ];
 
   useEffect(() => {
-    getProductList(1)
-  }, [])
+    getProductList(1);
+  }, []);
 
   const getProductList = (values) => {
-    console.log("vales>>", values)
-    fetch(`https://fobohwepapifbh.azurewebsites.net/api/product/GetAll?page=${values}`, {
-      method: "GET",
-    })
+    console.log("vales>>", values);
+    fetch(
+      `https://fobohwepapifbh.azurewebsites.net/api/product/GetAll?page=${values}`,
+      {
+        method: "GET",
+      }
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log("product lists --->", data);
         setProducts(data.data);
-        const array = createArrayWithNumber(data.last_page)
-        setPages(array)
+        const array = createArrayWithNumber(data.last_page);
+        setPages(array);
       })
       .catch((error) => console.log(error));
-  }
+  };
 
   const [filterAndSort, setFilterAndSort] = useState({
     filter: {
@@ -126,29 +137,30 @@ function Range() {
 
   const buttonClik = (type) => {
     switch (type) {
-      case 'next':
+      case "next":
         let newPage = page + 1;
-        setPage(page + 1)
-        getProductList(newPage)
+        setPage(page + 1);
+        getProductList(newPage);
         break;
-      case 'previous':
+      case "previous":
         if (page > 0) {
-          let newPage = page - 1
-          setPage(page > 0 ? page - 1 : 1)
-          getProductList(newPage)
+          let newPage = page - 1;
+          setPage(page > 0 ? page - 1 : 1);
+          getProductList(newPage);
         } else {
-          getProductList(1)
+          getProductList(1);
         }
         break;
       default:
         break;
     }
     // alert("button clikc",type)
-  }
+  };
   // console.log("range products", products);
   const handleCheckbox = (product) => {
     console.log("prod :->", product);
     setSelectedProducts(product);
+    console.log(selectedProducts, "setSelectedProducts");
     if (selectedProducts.includes(product)) {
       const newSelectedProducts = selectedProducts.filter(
         (prod) => prod !== product
@@ -165,139 +177,172 @@ function Range() {
         <div className="box-3 px-6 ">
           <SearchProduct setProducts={setProducts} products={products} />
         </div>
-        <CardBody className="overflow-scroll px-0">
-          <table className="w-full min-w-max table-auto text-left">
-            <thead>
-              <tr>
-                {TABLE_HEAD.map((head) => (
-                  <th
-                    key={head}
-                    className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
-                  >
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      {head}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {products.map(
-                (
-                  {
-                    title,
-                    skUcode,
-                    configuration,
-                    globalPrice,
-                    availableQty,
-                    stockStatus,
-                  },
-                  index,
-                ) => {
-                  const isLast = index === products.length - 1;
-                  const classes = isLast
-                    ? "p-4"
-                    : "p-4 border-b border-blue-gray-50";
+        <div className="pt-6 px-6 ">
+          <div className="box-4 relative overflow-x-auto overflow-y-auto h-80 no-scrollbar shadow-md sm:rounded-lg rounded-md border border-inherit bg-white">
+            <CardBody className="p-0">
+              <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead>
+                  <tr>
+                    <th scope="col" className="p-4 border-y">
+                      <div className="flex items-center">
+                        <input
+                          id="default-checkbox"
+                          type="checkbox"
+                          defaultValue=""
+                          className="w-4 h-4 text-darkGreen bg-gray-100 border-gray-300 rounded  dark:bg-gray-700 dark:border-gray-600"
+                        />
+                      </div>
+                    </th>
+                    <th scope="col" className="p-4 border-y">
+                      <div className="flex items-center"></div>
+                    </th>
+                    {TABLE_HEAD.map((head) => (
+                      <th
+                        key={head}
+                        className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                      >
+                        <Typography
+                          variant="small"
+                          className="font-medium leading-none text-base text-[#2B4447]"
+                        >
+                          {head}
+                        </Typography>
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product, index) => {
+                    const isLast = index === products.length - 1;
+                    const classes = isLast
+                      ? "p-4"
+                      : "p-4 border-b border-blue-gray-50 ";
 
-                  return (
-                    <tr key={name}>
-                      <td className={classes}>
-                        <div className="flex items-center gap-3">
-                          <input
-                            id="default-checkbox"
-                            type="checkbox"
-                            name={title}
-                            onClick={() => handleCheckbox(product)}
-                            className="w-4 h-4 text-darkGreen bg-gray-100 border-gray-300 rounded  dark:bg-gray-700 dark:border-gray-600"
-                          />
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-bold"
-                          >
-                            {title}
+                    return (
+                      <tr key={name}>
+                        <td className={classes}>
+                          <div className="flex items-center gap-3">
+                            <input
+                              id="default-checkbox"
+                              type="checkbox"
+                              name={product.title}
+                              onClick={() => handleCheckbox(product)}
+                              className="w-4 h-4 text-darkGreen bg-gray-100 border-gray-300 rounded  dark:bg-gray-700 dark:border-gray-600"
+                            />
+                          </div>
+                        </td>
+                        <td className={classes}>
+                          <div className="flex items-center gap-3">
+                            {product.productImage ? (
+                              <>
+                                <div className="">
+                                  <img
+                                    src={product.productImage}
+                                    alt=""
+                                    className="object-cover	"
+                                    style={{
+                                      borderRadius: "6px",
+                                      height: "40px",
+                                      width: "40px",
+                                    }}
+                                  />
+                                </div>
+                              </>
+                            ) : (
+                              <div
+                                className=" rounded-[6px] bg-[#D9D9D9]"
+                                style={{
+                                  height: "40px",
+                                  width: "40px",
+                                  borderRadius: "6px",
+                                }}
+                              ></div>
+                            )}
+                          </div>
+                        </td>
+                        <td className={classes}>
+                          <div className="flex items-center gap-3">
+                            <Typography className="font-medium	md:text-base text-sm text-[#637381]">
+                              {product.title}
+                            </Typography>
+                          </div>
+                        </td>
+                        <td className={classes}>
+                          <Typography className="font-normal md:text-base text-sm text-[#637381]">
+                            {product.skUcode}
                           </Typography>
-                        </div>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {skUcode}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {configuration}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {globalPrice}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {availableQty}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {stockStatus}
-                        </Typography>
-                      </td>
-                    </tr>
-                  );
-                },
-              )}
-            </tbody>
-          </table>
-        </CardBody>
+                        </td>
+                        <td className={`${classes} w-44`}>
+                          <Typography className="font-normal md:text-base text-sm text-[#637381]">
+                            {product.configuration}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <Typography className="font-normal md:text-base text-sm text-[#637381]">
+                            {product.globalPrice}
+                          </Typography>
+                        </td>
+                        <td className={classes}>
+                          <div
+                            className="bg-[#EDF7F1] py-1 px-3.5	rounded-[30px]"
+                            style={{
+                              background: "#EDF7F1",
+                              paddingLeft: "0.875rem",
+                              paddingRight: "0.875rem",
+                              borderRadius: "30px",
+                            }}
+                          >
+                            <Typography className="font-normal md:text-base text-sm text-[#637381] text-center ">
+                              {product.availableQty}
+                            </Typography>
+                          </div>
+                        </td>
+                        <td className={classes}>
+                          <Typography className="font-normal md:text-base text-sm text-[#637381]">
+                            {product.stockStatus}
+                          </Typography>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </CardBody>
 
-        <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Button onClick={() => buttonClik('previous')} variant="outlined" size="sm" >
-            Previous
-          </Button>
-          <div className="flex items-center gap-2">
-            <div>
-              {pages.map((page, index) => {
-                return (
-                  <div
-                    onClick={() => navigate(`/dashboard/prooducts/${index + 1}`)}
-                    className="hidden px-4 py-2 mx-1 text-green  bg-white rounded-md sm:inline  dark:text-gray-200  table-pagination "
-                  >
-                    {index + 1}
-                  </div>
-                );
-              })}
-            </div>
+            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+              <Button
+                onClick={() => buttonClik("previous")}
+                variant="outlined"
+                size="sm"
+              >
+                Previous
+              </Button>
+              <div className="flex items-center gap-2">
+                <div>
+                  {pages.map((page, index) => {
+                    return (
+                      <div
+                        onClick={() =>
+                          navigate(`/dashboard/prooducts/${index + 1}`)
+                        }
+                        className="hidden px-4 py-2 mx-1 text-green  bg-white rounded-md sm:inline  dark:text-gray-200  table-pagination "
+                      >
+                        {index + 1}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <Button
+                onClick={() => buttonClik("next")}
+                variant="outlined"
+                size="sm"
+              >
+                Next
+              </Button>
+            </CardFooter>
           </div>
-          <Button onClick={() => buttonClik('next')} variant="outlined" size="sm">
-            Next
-          </Button>
-        </CardFooter>
+        </div>
         {/* <div className="box-4 pt-6 px-6 relative">
           <div className="relative overflow-x-auto overflow-y-auto h-80 no-scrollbar shadow-md sm:rounded-lg rounded-md border border-inherit bg-white">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
