072b75fc3ae7124439989b7b489dbc4f5cb290c6
nikit kumawat <nikit.kumawat@nextdynamix.com>
2023-08-17 10:17:02 +0530

    add

--- foboh-frontend/products/src/helpers/createArrayWithNumbers.js
+++ foboh-frontend/products/src/helpers/createArrayWithNumbers.js
@@ -2,7 +2,7 @@ function createArrayWithNumber(num) {
     const resultArray = [];
   
     for (let i = 0; i < num; i++) {
-      resultArray.push(i);
+      resultArray.(i);
     }
   
     return resultArray;
