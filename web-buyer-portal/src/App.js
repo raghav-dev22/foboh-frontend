f6870969b612e2e4c9edb40071292c47f84a185c
Raghav Saini <raghavsaini30795@gmail.com>
2023-08-21 16:53:49 +0530

    add redux toolkit in web buyer portal

--- web-buyer-portal/src/App.js
+++ web-buyer-portal/src/App.js
@@ -3,20 +3,24 @@ import Login from "./loginRegister/Login";
 import CreateAccount from "./CreateAccount/CreateAccount";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./loginRegister/Signup";
+import { Provider } from "react-redux";
 import Verifyemail from "./loginRegister/Verifyemail";
 import HomePage from "./desktop/HomePage";
+import store from "./store";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
-        <Route path="/verify-email" element={<Verifyemail />} />
-        <Route path="/create-account" element={<CreateAccount />} />
-        <Route path="/home-page" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/sign-up" element={<Signup />} />
+          <Route path="/verify-email" element={<Verifyemail />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/home-page" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ web-buyer-portal/src/store.js
@@ -0,0 +1,9 @@
+// src/app/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlice';
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
