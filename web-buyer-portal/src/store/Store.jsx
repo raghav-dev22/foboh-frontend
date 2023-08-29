d0a1290ab7ca2a4547cb90677262369465c4eac5
Raghav Saini <raghavsaini30795@gmail.com>
2023-08-29 17:26:02 +0530

    make correction in product range search box

--- web-buyer-portal/src/store/Store.jsx
+++ web-buyer-portal/src/store/Store.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./CartSlice";
 import ProductReducer from "./ProductSlice";
+import buyerReducer from "../slices/buyerSlice";
 const Store = configureStore({
   reducer: {
     cart: cartReducer,
     product: ProductReducer,
+    buyer : buyerReducer
   },
 });
 export default Store;
