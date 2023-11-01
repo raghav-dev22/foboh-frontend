b66ad7c795e92c130c8388d02bf71f6d80baa240
Raghav Saini <raghavsaini30795@gmail.com>
2023-11-01 17:49:09 +0530

    add get order details api

--- foboh-frontend/supplier_order_management/src/components/orderDetailHeader/OrderDetailHeader.jsx
+++ foboh-frontend/supplier_order_management/src/components/orderDetailHeader/OrderDetailHeader.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { Select } from "antd";
+import { fomatDateAndTime } from "../../helpers/formatDateAndTime";
 const handleChange = (value) => {
   console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
 };
 
-const OrderDetailHeader = () => {
+const OrderDetailHeader = ({ orderAdressDetails }) => {
   return (
     <>
       <div className="flex justify-between mb-5 py-4">
@@ -15,17 +16,17 @@ const OrderDetailHeader = () => {
           </div>
           <div className="flex flex-col gap-2">
             <h4 className="text-lg font-semibold text-[#212B36]">
-              Orders #15478
+              Orders {orderAdressDetails?.orderId}
             </h4>
             <h5 className="font-medium text-base text-[#212B36]">
-              Business Full Name
+              {orderAdressDetails?.customerName}
             </h5>
-            <h5 className="font-medium text-base text-[#212B36]">
+            {/* <h5 className="font-medium text-base text-[#212B36]">
               Business address
-            </h5>
+            </h5> */}
           </div>
           <h5 className="text-base font-medium text-[#637381] ">
-            19 Sep 2023 at 6:23
+            {fomatDateAndTime(orderAdressDetails?.orderEntryDate)}
           </h5>
           <Select
             labelInValue
@@ -56,9 +57,6 @@ const OrderDetailHeader = () => {
               },
             ]}
           />
-          <h5 className="text-base font-medium text-[#637381]">
-            Channel: text msg
-          </h5>
         </div>
         <button className="h-fit bg-[#147D73] rounded-[6px] text-white text-base font-semibold py-2 px-4">
           New
@@ -66,35 +64,6 @@ const OrderDetailHeader = () => {
       </div>
     </>
   );
-  <div className="flex justify-between mb-5">
-    <div className=" flex justify-start items-start gap-5">
-      <div className="border border-[#EDEFF1] bg-white rounded-[6px] p-2">
-        <ChevronLeftIcon />
-      </div>
-      <div className="flex flex-col gap-2">
-        <h4 className="text-lg font-semibold text-[#212B36]">Orders #15478</h4>
-        <h5 className="font-medium text-base text-[#212B36]">
-          Business Full Name
-        </h5>
-        <h5 className="font-medium text-base text-[#212B36]">
-          Business address
-        </h5>
-      </div>
-      <h5 className="text-base font-medium text-[#637381] ">
-        19 Sep 2023 at 6:23
-      </h5>
-      <h5 className="text-base font-medium text-[#637381]">
-        Payment:{" "}
-        <span className=" text-base font-medium  text-[#009900]"> Paid</span>{" "}
-      </h5>
-      <h5 className="text-base font-medium text-[#637381]">
-        Channel: text msg
-      </h5>
-    </div>
-    <button className="h-fit bg-[#147D73] rounded-[6px] text-white text-base font-semibold py-2 px-4">
-      New
-    </button>
-  </div>;
 };
 
 export default OrderDetailHeader;
