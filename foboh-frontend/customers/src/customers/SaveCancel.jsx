fe913cd4e90de32dc5066e28b8fca4888f238347
sakshi jain <sjain302086@gmail.com>
2023-11-07 09:46:06 +0530

    add popup

--- foboh-frontend/customers/src/customers/SaveCancel.jsx
+++ foboh-frontend/customers/src/customers/SaveCancel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SaveCancel = ({ handleCancel, onFinalSubmit }) => {
+const SaveCancel = ({ handleCancel, onFinalSubmit, handleCustomerTiles }) => {
   return (
     <>
       <div className=" 2xl:mx-auto absolute z-50 top-0 right-0 left-0">
@@ -9,7 +9,7 @@ const SaveCancel = ({ handleCancel, onFinalSubmit }) => {
             <nav className="flex h-[65px] items-center justify-end gap-5 ">
               <button
                 className="rounded-md	bg-white px-6	py-2.5 text-green text-base	font-medium	"
-                onClick={handleCancel}
+                onClick={handleCustomerTiles}
               >
                 Cancel
               </button>
