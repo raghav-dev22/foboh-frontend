018561277613f98b200c8b88236d1881bd1a9827
sakshi jain <sjain302086@gmail.com>
2023-11-15 10:09:35 +0530

    add

--- foboh-frontend/products/src/product/ViewProduct.jsx
+++ foboh-frontend/products/src/product/ViewProduct.jsx
@@ -2157,6 +2157,12 @@ function ViewProduct() {
                             onBlur={handleBlur}
                             type="text"
                             placeholder="$330.00"
+                            onKeyPress={(e) => {
+                              const isValidKey = /[0-9.]/.test(e.key); // Test if the pressed key is a number
+                              if (!isValidKey) {
+                                e.preventDefault(); // Prevent input of non-numeric characters
+                              }
+                            }}
                           />
                           {errors.salePrice && touched.salePrice && (
                             <p className="mt-2 mb-2 text-red-500 text-xs	font-normal	">
