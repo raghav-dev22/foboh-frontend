1e26226e294122b3fa16f30aa024ac973c7d72c3
sakshi jain <sjain302086@gmail.com>
2023-12-26 11:01:52 +0530

    add chnages

--- foboh-frontend/supplier_order_management/src/components/orders/TotalOrders.jsx
+++ foboh-frontend/supplier_order_management/src/components/orders/TotalOrders.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { getAllOrderDetails } from "../../helpers/dashbordApiModule";
 import { useState } from "react";
 import { formatPrice } from "../../helpers/formatPrice";
+import { Skeleton } from "antd";
 
 const TotalOrders = () => {
   const [totalOrders, setTotalOrders] = useState();
@@ -46,131 +47,165 @@ const TotalOrders = () => {
   return (
     <>
       <div className="grid lg:grid-cols-2 grid-cols-1  py-5 justify-between gap-4 items-center">
-        <div className="bg-white relative  px-3 py-2  custom-shadow rounded-[8px] flex justify-between">
-          <div className="absolute top-[11px] left-[50%] bg-[#E7E7E7] h-[80%] w-[1px]"></div>
-          <div className="w-full py-3 px-4 ">
-            <h5 className="text-lg font-bold text-[#212B36]">
-              {allOrders?.totalOrders ? allOrders?.totalOrders : "00.00"}
-            </h5>
-            <div className="flex justify-between items-center pt-3 ">
-              <p className="text-sm font-medium text-[#637381]">Total Orders</p>
-              <div className="flex justify-center items-center gap-2 ">
-                <p
-                  className={`text-sm font-medium ${
-                    allOrders?.totalOrdersPercentage === 0
-                      ? "text-[#637381]"
-                      : allOrders?.totalOrdersPercentage > 0
-                      ? "text-[#009900]"
-                      : "text-[#DC3545]"
-                  }`}
-                >
-                  {allOrders?.totalOrdersPercentage?.toFixed(2)}%
+        {allOrders?.totalOrders ? (
+          <div className="bg-white relative  px-3 py-2  custom-shadow rounded-[8px] flex justify-between">
+            <div className="absolute top-[11px] left-[50%] bg-[#E7E7E7] h-[80%] w-[1px]"></div>
+            <div className="w-full py-3 px-4 ">
+              <h5 className="text-lg font-bold text-[#212B36]">
+                {allOrders?.totalOrders ? allOrders?.totalOrders : "00.00"}
+              </h5>
+              <div className="flex justify-between items-center pt-3 ">
+                <p className="text-sm font-medium text-[#637381]">
+                  Total Orders
                 </p>
-                {allOrders?.totalOrdersPercentage > 0 ? (
-                  <ArrowUpwardIcon style={{ fill: "#009900", width: "20px" }} />
-                ) : allOrders?.totalOrdersPercentage < 0 ? (
-                  <ArrowDownwardIcon
-                    style={{ fill: "#DC3545", width: "20px" }}
-                  />
-                ) : null}
+                <div className="flex justify-center items-center gap-2 ">
+                  <p
+                    className={`text-sm font-medium ${
+                      allOrders?.totalOrdersPercentage === 0
+                        ? "text-[#637381]"
+                        : allOrders?.totalOrdersPercentage > 0
+                        ? "text-[#009900]"
+                        : "text-[#DC3545]"
+                    }`}
+                  >
+                    {allOrders?.totalOrdersPercentage?.toFixed(2)}%
+                  </p>
+                  {allOrders?.totalOrdersPercentage > 0 ? (
+                    <ArrowUpwardIcon
+                      style={{ fill: "#009900", width: "20px" }}
+                    />
+                  ) : allOrders?.totalOrdersPercentage < 0 ? (
+                    <ArrowDownwardIcon
+                      style={{ fill: "#DC3545", width: "20px" }}
+                    />
+                  ) : null}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full py-3 px-4">
-            <h5 className="text-lg font-bold text-[#212B36]">
-              {allOrders?.openOrders ? allOrders?.openOrders : "00.00"}
-            </h5>
-            <div className="flex justify-between items-center pt-3 ">
-              <p className="text-sm font-medium text-[#637381]">Open Orders</p>
-              <div className="flex justify-center items-center gap-2">
-                <p
-                  className={`text-sm font-medium ${
-                    allOrders?.openOrdersPercentage === 0
-                      ? "text-[#637381]"
-                      : allOrders?.openOrdersPercentage > 0
-                      ? "text-[#009900]"
-                      : "text-[#DC3545]"
-                  }`}
-                >
-                  {allOrders?.openOrdersPercentage?.toFixed(2)}%
+            <div className="w-full py-3 px-4">
+              <h5 className="text-lg font-bold text-[#212B36]">
+                {allOrders?.openOrders ? allOrders?.openOrders : "00.00"}
+              </h5>
+              <div className="flex justify-between items-center pt-3 ">
+                <p className="text-sm font-medium text-[#637381]">
+                  Open Orders
                 </p>
+                <div className="flex justify-center items-center gap-2">
+                  <p
+                    className={`text-sm font-medium ${
+                      allOrders?.openOrdersPercentage === 0
+                        ? "text-[#637381]"
+                        : allOrders?.openOrdersPercentage > 0
+                        ? "text-[#009900]"
+                        : "text-[#DC3545]"
+                    }`}
+                  >
+                    {allOrders?.openOrdersPercentage?.toFixed(2)}%
+                  </p>
 
-                {openOrders > 0 ? (
-                  <ArrowUpwardIcon style={{ fill: "#009900", width: "20px" }} />
-                ) : openOrders < 0 ? (
-                  <ArrowDownwardIcon
-                    style={{ fill: "#DC3545", width: "20px" }}
-                  />
-                ) : null}
+                  {openOrders > 0 ? (
+                    <ArrowUpwardIcon
+                      style={{ fill: "#009900", width: "20px" }}
+                    />
+                  ) : openOrders < 0 ? (
+                    <ArrowDownwardIcon
+                      style={{ fill: "#DC3545", width: "20px" }}
+                    />
+                  ) : null}
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="bg-white relative  px-3 py-2  custom-shadow rounded-[8px] flex justify-between">
-          <div className="absolute top-[11px] left-[50%] bg-[#E7E7E7] h-[80%] w-[1px]"></div>
-          <div className="w-full py-3 px-4 ">
-            <h5 className="text-lg font-bold text-[#212B36]">
-              {allOrders?.paidOrders
-                ? formatPrice(allOrders?.paidOrders)
-                : "00.00"}
-            </h5>
-            <div className="flex justify-between items-center pt-3 ">
-              <p className="text-sm font-medium text-[#637381]">Paid Orders</p>
-              <div className="flex justify-center items-center gap-2 ">
-                <p
-                  className={`text-sm font-medium ${
-                    allOrders?.paidOrdersPercentage === 0
-                      ? "text-[#637381]"
-                      : allOrders?.paidOrdersPercentage > 0
-                      ? "text-[#009900]"
-                      : "text-[#DC3545]"
-                  }`}
-                >
-                  {allOrders?.paidOrdersPercentage?.toFixed(2)}%
+        ) : (
+          <div className="bg-white relative  px-3 py-6  custom-shadow rounded-[8px]">
+            <Skeleton
+              paragraph={{ rows: 1 }}
+              active
+              className="custom-skeleton-header"
+            />
+          </div>
+        )}
+        {allOrders?.paidOrders ? (
+          <div className="bg-white relative  px-3 py-2  custom-shadow rounded-[8px] flex justify-between">
+            <div className="absolute top-[11px] left-[50%] bg-[#E7E7E7] h-[80%] w-[1px]"></div>
+            <div className="w-full py-3 px-4 ">
+              <h5 className="text-lg font-bold text-[#212B36]">
+                {allOrders?.paidOrders
+                  ? formatPrice(allOrders?.paidOrders)
+                  : "00.00"}
+              </h5>
+              <div className="flex justify-between items-center pt-3 ">
+                <p className="text-sm font-medium text-[#637381]">
+                  Paid Orders
                 </p>
-                {paidOrders > 0 ? (
-                  <ArrowUpwardIcon style={{ fill: "#009900", width: "20px" }} />
-                ) : paidOrders < 0 ? (
-                  <ArrowDownwardIcon
-                    style={{ fill: "#DC3545", width: "20px" }}
-                  />
-                ) : null}
+                <div className="flex justify-center items-center gap-2 ">
+                  <p
+                    className={`text-sm font-medium ${
+                      allOrders?.paidOrdersPercentage === 0
+                        ? "text-[#637381]"
+                        : allOrders?.paidOrdersPercentage > 0
+                        ? "text-[#009900]"
+                        : "text-[#DC3545]"
+                    }`}
+                  >
+                    {allOrders?.paidOrdersPercentage?.toFixed(2)}%
+                  </p>
+                  {paidOrders > 0 ? (
+                    <ArrowUpwardIcon
+                      style={{ fill: "#009900", width: "20px" }}
+                    />
+                  ) : paidOrders < 0 ? (
+                    <ArrowDownwardIcon
+                      style={{ fill: "#DC3545", width: "20px" }}
+                    />
+                  ) : null}
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full py-3 px-4">
-            <h5 className="text-lg font-bold text-[#212B36]">
-              {allOrders?.unpaidOrders
-                ? formatPrice(allOrders?.unpaidOrders)
-                : "00.00"}
-            </h5>
-            <div className="flex justify-between items-center pt-3 ">
-              <p className="text-sm font-medium text-[#637381]">
-                Unpaid Orders
-              </p>
-              <div className="flex justify-center items-center gap-2">
-                <p
-                  className={`text-sm font-medium ${
-                    allOrders?.unpaidOrdersPercentage === 0
-                      ? "text-[#637381]"
-                      : allOrders?.unpaidOrdersPercentage > 0
-                      ? "text-[#009900]"
-                      : "text-[#DC3545]"
-                  }`}
-                >
-                  {allOrders?.unpaidOrdersPercentage?.toFixed(2)}%
+            <div className="w-full py-3 px-4">
+              <h5 className="text-lg font-bold text-[#212B36]">
+                {allOrders?.unpaidOrders
+                  ? formatPrice(allOrders?.unpaidOrders)
+                  : "00.00"}
+              </h5>
+              <div className="flex justify-between items-center pt-3 ">
+                <p className="text-sm font-medium text-[#637381]">
+                  Unpaid Orders
                 </p>
-                {unpaidOrders > 0 ? (
-                  <ArrowUpwardIcon style={{ fill: "#009900", width: "20px" }} />
-                ) : unpaidOrders < 0 ? (
-                  <ArrowDownwardIcon
-                    style={{ fill: "#DC3545", width: "20px" }}
-                  />
-                ) : null}
+                <div className="flex justify-center items-center gap-2">
+                  <p
+                    className={`text-sm font-medium ${
+                      allOrders?.unpaidOrdersPercentage === 0
+                        ? "text-[#637381]"
+                        : allOrders?.unpaidOrdersPercentage > 0
+                        ? "text-[#009900]"
+                        : "text-[#DC3545]"
+                    }`}
+                  >
+                    {allOrders?.unpaidOrdersPercentage?.toFixed(2)}%
+                  </p>
+                  {unpaidOrders > 0 ? (
+                    <ArrowUpwardIcon
+                      style={{ fill: "#009900", width: "20px" }}
+                    />
+                  ) : unpaidOrders < 0 ? (
+                    <ArrowDownwardIcon
+                      style={{ fill: "#DC3545", width: "20px" }}
+                    />
+                  ) : null}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="bg-white relative  px-3 py-6  custom-shadow rounded-[8px]">
+            <Skeleton
+              paragraph={{ rows: 1 }}
+              active
+              className="custom-skeleton-header"
+            />
+          </div>
+        )}
       </div>
     </>
   );
